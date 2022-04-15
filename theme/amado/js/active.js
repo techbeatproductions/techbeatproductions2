7858a2b05b0c77cd0790ff1374b7e32167803755
techbeatproductions <103606897+techbeatproductions@users.noreply.github.com>
2022-04-15 16:16:33 +0300

    del
    del

--- theme/amado/js/active.js
+++ theme/amado/js/active.js
@@ -94,26 +94,6 @@
         $.preventDefault();
     });
 
-    // :: 11.0 Slider Range Price Active Code
-    $('.slider-range-price').each(function() {
-        var min = jQuery(this).data('min');
-        var max = jQuery(this).data('max');
-        var unit = jQuery(this).data('unit');
-        var value_min = jQuery(this).data('value-min');
-        var value_max = jQuery(this).data('value-max');
-        var label_result = jQuery(this).data('label-result');
-        var t = $(this);
-        $(this).slider({
-            range: true,
-            min: min,
-            max: max,
-            values: [value_min, value_max],
-            slide: function(event, ui) {
-                var result = label_result + " " + unit + ui.values[0] + ' - ' + unit + ui.values[1];
-                console.log(t);
-                t.closest('.slider-range').find('.range-price').html(result);
-            }
-        });
-    });
+
 
 })(jQuery);
